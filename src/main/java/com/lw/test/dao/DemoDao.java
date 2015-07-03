08a379a33220e33fd44ecd744b5971a5a7c114ed
Gliwei <bjguoliwei@163.com>
2015-07-03 21:19:05 +0800

    修改启动端口为8080；DemoDao BUGFIXED

--- src/main/java/com/lw/test/dao/DemoDao.java
+++ src/main/java/com/lw/test/dao/DemoDao.java
@@ -2,14 +2,18 @@ package com.lw.test.dao;
 
 import java.util.Date;
 
+import org.springframework.stereotype.Component;
+
 import com.lw.test.entity.DemoEntity;
 
+@Component
 public class DemoDao {
 	public DemoEntity findOne(){
 		DemoEntity entity = new DemoEntity();
 		entity.setId(1);
 		entity.setName("Conan");
 		entity.setLastModifyDate(new Date());
+		System.out.println("WOWOWOWOW");
 		return entity;
 	}
 }
