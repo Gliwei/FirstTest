ca1a13adb0af28631ead57087c313e963b8a32ef
GPC <GPC@DESKTOP-6IA3A9R>
2017-03-26 04:04:36 +0800

    代碼沒有提交成功啊

---
+++ src/main/java/com/lw/solr/vo/SpecVo.java
@@ -0,0 +1,22 @@
+package com.lw.solr.vo;
+
+public class SpecVo {
+	private String name;
+	private Long count;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getCount() {
+		return this.count;
+	}
+
+	public void setCount(Long count) {
+		this.count = count;
+	}
+}
